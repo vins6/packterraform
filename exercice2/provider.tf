28e027ce174bc43bb1ce97176745710c1afb69da
vins6 <vincent.thomas.martin@gmail.com>
2026-04-01 09:15:51 +0200

    update date

---
+++ exercice2/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.40.0"
+    }
+  }
+}
+# Configure the AWS Provider
+provider "aws" {
+  # Change this region to match your default region!!!!!!!!!!!!!
+  region = "eu-west-3"
+  shared_credentials_files = ["/home/vagrant/.aws/credentials"]
+}
