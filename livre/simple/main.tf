d047b68c4b02910142dce7333e784bc47b93fdca
vins6 <vincent.thomas.martin@gmail.com>
2026-06-30 17:06:27 +0200

    appliweb

--- livre/simple/main.tf
+++ livre/simple/main.tf
@@ -5,5 +5,15 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-0e1c4170d9c01184b"
     instance_type = "t3.micro"
+
+    user_data_replace_on_change= <<- EOF
+    #/bin/bash
+    echo "Hello World" > index.html
+    nohup busybox httpd -f -p 8080 &
+    EOF
+    
+    tags {
+        Name = "terraform-example"
+    }
   
 }
